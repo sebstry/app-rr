830526243fe8332e3023e81b940f1773abf61664
Sebastian Stryczek <sebastian.stryczek@hotmail.com>
2020-06-28 20:27:12 +0200

    Updates

---
+++ precache-manifest.1342314c6dd9005417f15f1146a360b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b22224b3adacc4137567",
+    "url": "/app-rr/static/css/main.5c44a952.chunk.css"
+  },
+  {
+    "revision": "b22224b3adacc4137567",
+    "url": "/app-rr/static/js/main.c4ec2c9d.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "e06ff08ce8035e1a1b2f",
+    "url": "/app-rr/static/js/2.ec904aca.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "ee4601cd0fff04e38a5ad1946468dedb",
+    "url": "/app-rr/index.html"
+  }
+];
